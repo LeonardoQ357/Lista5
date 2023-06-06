92844356c8ed62b322e28f16e1b2288b7a4abf68
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-06-06 09:24:53 -0300

    excluir

---
+++ excluir_agenda.php
@@ -0,0 +1,16 @@
+<?php
+    include('conexao.php');
+
+    $slq = "DELETE FROM agenda WHERE id_agenda='". $_GET['id_agenda'] ."'";
+    $result = mysqli_query($con, $sql);
+
+    if($result){
+        header('Location:listar_agenda.php');
+    }else{
+        echo "Erro ao tentar excluir dados: ". $mysql_error($con) ."!";
+        echo
+        "<div>
+            <a href='listar_usuarios.php'>Voltar</a>
+        </div>"; 
+    }
+?>
