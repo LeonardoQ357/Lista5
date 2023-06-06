f0dc87e1f720478e9f699b8cd87da5ea0b266f1a
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-06-06 07:26:26 -0300

    bo

---
+++ Banco/lista5.sql
@@ -0,0 +1,15 @@
+create database lista5;
+
+create table agenda(
+	id_agenda int primary key not null auto_increment,
+	nome varchar(50) not null,
+	apelidao varchar(40) not null,
+	endereco varchar(70) not null,
+    bairro varchar(50) not null,
+    cidade varchar(50) not null,
+    estado varchar(2) not null,
+    telefone varchar(15) not null,
+    celular varchar(15) not null,
+    email varchar(70) not null,
+    dt_cadastro date not null);
+    
