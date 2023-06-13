cee77eccd62c351ebc207cbe7b9764baa1304cb9
leonardoQ357 <leonardo.quederoli.leme@hotmail.com>
2023-06-13 10:20:56 -0300

    bootstrap

--- listar_agenda.php
+++ listar_agenda.php
@@ -11,6 +11,35 @@
     <title>Lista de agendas</title>
 </head>
 <body>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-expand-lg">
+            <span class="navbar-brand">Agenda</span>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-expanded="false" aria-controls="conteudoNavbarSuportado"  aria-label="Alterna navegação">
+                <span class="navbar-toggler-icon"></span>
+              </button> 
+
+            <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">    
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="cadastro_agenda.html">Cadastrar</a>
+                    </li>
+
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Listar</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
     <?php
         include('conexao.php');
 
